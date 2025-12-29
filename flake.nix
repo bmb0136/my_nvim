87b7e65d3d2dfd93a6228d64a212a1d8d50a01b8
Brandon Buckley <brandonmb0136@gmail.com>
2025-12-28 22:42:08 -0500

    Remove unnecessary systems from flake

--- flake.nix
+++ flake.nix
@@ -18,13 +18,7 @@
           "x86_64-linux"
           "aarch64-linux"
           "x86_64-darwin"
-          "armv6l-linux"
-          "armv7l-linux"
-          "i686-linux"
           "aarch64-darwin"
-          "powerpc64le-linux"
-          "riscv64-linux"
-          "x86_64-freebsd"
         ];
         flake = {
           lib = rec {
